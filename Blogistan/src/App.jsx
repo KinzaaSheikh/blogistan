92fc87f8ff2cb8118157ebfc181e1069d3cd6a8f
Kinza <kinzasheikhsiddiqui@gmail.com>
2024-01-10 21:48:59 +0500

    Recent updates

--- Blogistan/src/App.jsx
+++ Blogistan/src/App.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth.js'
+import { login, logout } from './store/authSlice.js'
+import { Header, Footer } from './components'
+import { Outlet }  from 'react-router-dom'
 
 function App() {
-  return (
-    <>
-      <h1>Blogging in AppWrite</h1>
-    </>
-  )
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
