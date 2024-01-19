0a8629fe144bf41298ab274414e170485864902e
Kinza <kinzasheikhsiddiqui@gmail.com>
2024-01-19 18:52:55 +0500

    Started building post form

---
+++ Blogistan/src/components/post-form/PostForm.jsx
@@ -0,0 +1,16 @@
+import React, { useCallback } from 'react'
+import { useForm } from "react-hook-form";
+import { Button, Input, RTE, Select } from "..";
+import appwriteService from '../../appwrite/conf.js'
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function PostForm({ post }) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
